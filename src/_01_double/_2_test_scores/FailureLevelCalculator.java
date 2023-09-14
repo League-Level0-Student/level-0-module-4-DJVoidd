0a49ae527059e6c2ba78a2c1c710b34b9a9c3461
league <maddoxseul@sbsd.net>
2023-09-13 19:56:38 -0700

    I made my parents proud with my test scores (maybe)

--- src/_01_double/_2_test_scores/FailureLevelCalculator.java
+++ src/_01_double/_2_test_scores/FailureLevelCalculator.java
@@ -1,7 +1,19 @@
 package _01_double._2_test_scores;
 
+import javax.swing.JOptionPane;
+
 public class FailureLevelCalculator {
 public static void main(String[] args) {
-	
+	String failure = JOptionPane.showInputDialog("Input your grade");
+	double failurint = Double.parseDouble(failure);
+	if (failurint >= 61 && failurint <= 70) {
+		JOptionPane.showMessageDialog(null, "You barely passed");
+		if (failurint <= 60) {
+			JOptionPane.showMessageDialog(null, "You failed bozo");
+			if (failurint >= 71 && failurint <= 80) {
+				
+			}
+		}
+	}
 }
 }
