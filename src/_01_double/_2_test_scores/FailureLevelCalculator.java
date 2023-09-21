dda035dd049e0292f34f277975357f80961ab5ab
league <maddoxseul@sbsd.net>
2023-09-20 19:57:02 -0700

    The game is over

--- src/_01_double/_2_test_scores/FailureLevelCalculator.java
+++ src/_01_double/_2_test_scores/FailureLevelCalculator.java
@@ -8,12 +8,24 @@ public static void main(String[] args) {
 	double failurint = Double.parseDouble(failure);
 	if (failurint >= 61 && failurint <= 70) {
 		JOptionPane.showMessageDialog(null, "You barely passed");
-		if (failurint <= 60) {
-			JOptionPane.showMessageDialog(null, "You failed bozo");
-			if (failurint >= 71 && failurint <= 80) {
-				
-			}
-		}
 	}
+		if (failurint <= 60) {
+			JOptionPane.showMessageDialog(null, "You failed");
+		}
+			if (failurint >= 71 && failurint <= 80) {
+				JOptionPane.showMessageDialog(null, "That's average");	
+			}
+			if (failurint >= 81 && failurint <= 90) {
+				JOptionPane.showMessageDialog(null, "That's pretty good!");	
+			}
+				if (failurint >= 91 && failurint <= 99) {
+					JOptionPane.showMessageDialog(null, "Impressive!");
+				}
+					if (failurint == 100) {
+						JOptionPane.showMessageDialog(null, "Go you!");	
+					}
+						if (failurint >= 101) {
+							JOptionPane.showMessageDialog(null, "...");	
+						}
 }
 }
--- src/_02_boolean/_3_game_over/GameOver.java
+++ src/_02_boolean/_3_game_over/GameOver.java
@@ -9,14 +9,19 @@ public class GameOver {
 		boolean gameOver = false;
 		
 		// 1. Ask the user if the game is over and save their answer.
-		
+		while (gameOver = false) {
+		String cont = JOptionPane.showInputDialog("Is the game over?");
 		// 2. If they answer "yes" change gameOver to true.
-		
 		// 3. Surround steps 1-2 in a while loop that runs as long as
 		//    gameOver is false. Hint: Use the ! operator.
-		
+		if (cont.equals("yes")) {
+			gameOver = true;
+		}
+		}
 		// 4. Tell the user the game is over outside the while loop.
-		
+		while (gameOver = true) {
+		JOptionPane.showMessageDialog(null, "The game is over");
+	}
 	}
 
 }
