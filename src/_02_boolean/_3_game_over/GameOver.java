7a769821c2257cdc5a6d799ff041c09a93096bc4
league <maddoxseul@sbsd.net>
2023-09-27 20:02:32 -0700

    ERR_GAME_OVER

--- src/_02_boolean/_3_game_over/GameOver.java
+++ src/_02_boolean/_3_game_over/GameOver.java
@@ -9,8 +9,8 @@ public class GameOver {
 		boolean gameOver = false;
 		
 		// 1. Ask the user if the game is over and save their answer.
-		while (gameOver = false) {
-		String cont = JOptionPane.showInputDialog("Is the game over?");
+		while (gameOver == false) {
+			String cont = JOptionPane.showInputDialog("Is the game over?");
 		// 2. If they answer "yes" change gameOver to true.
 		// 3. Surround steps 1-2 in a while loop that runs as long as
 		//    gameOver is false. Hint: Use the ! operator.
@@ -19,9 +19,7 @@ public class GameOver {
 		}
 		}
 		// 4. Tell the user the game is over outside the while loop.
-		while (gameOver = true) {
 		JOptionPane.showMessageDialog(null, "The game is over");
-	}
 	}
 
 }
